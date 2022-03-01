e103165f8f7996b6dbc41960d909bc4e56fc31fd
RathiAnkxrx <ankurrathi15@gmail.com>
2022-03-01 22:25:29 +0530

    initial comment

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const navlinks = document.querySelectorAll(".nav__link");
 const crewslink = document.querySelectorAll(".slider");
 const technologylink = document.querySelectorAll(".technology__nav-link");
+const hamburger = document.querySelector(".nav__hamburger");
+const header = document.querySelector(".header");
+const close = document.querySelector(".close");
 let url = window.location.pathname;
 let urlArray = url.split("/");
 let particularPath = urlArray.at("-1").split("-").at(-1);
 let overAllPath = urlArray.at("-1").split("-").at(0);
 
 const addActive = function (parent, path) {
-  // if ((path = "")) {
-  //   console.log(navlinks);
-  //   navlinks[0].classList.add("active");
-  // }
-  // parent.forEach((nav) => {
-  //   console.log(nav, path, "entered");
-  //   if (nav.href.includes(path)) {
-  //     nav.classList.add("active");
-  //   }
-  // });
+  parent.forEach((nav) => {
+    console.log(nav, path, "entered");
+    if (nav.href.includes(path)) {
+      nav.classList.add("active");
+    }
+  });
 };
 const displayLine = () => {
   if (particularPath === "") {
@@ -32,6 +31,14 @@ const displayLine = () => {
     }
   });
   addActive(technologylink, particularPath);
-  // addActive(crewslink, particularPath);
+  addActive(crewslink, particularPath);
 };
 displayLine();
+
+hamburger.addEventListener("click", () => {
+  header.classList.add("active");
+});
+
+close.addEventListener("click", () => {
+  header.classList.remove("active");
+});
